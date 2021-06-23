71bbeeed521c4e40bde218c2546206e2d8e4ea61
Leigh McCulloch <leigh@stellar.org>
2021-06-23 19:28:48 +0000

    sdk/state: fix TestProposePayment_validAsset test

--- sdk/state/open_test.go
+++ sdk/state/open_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestProposePayment_valid_asset(t *testing.T) {
+func TestProposePayment_validAsset(t *testing.T) {
 	localSigner := keypair.MustRandom()
 	remoteSigner := keypair.MustRandom()
 	localEscrowAccount := &EscrowAccount{
@@ -33,13 +33,12 @@ func TestProposePayment_valid_asset(t *testing.T) {
 	require.NoError(t, err)
 
 	invalidCredit := CreditAsset{}
-	_, err = sendingChannel.ProposeOpen(OpenParams{Asset: invalidCredit, AssetLimit: ""})
-	require.Error(t, err)
+	_, err = sendingChannel.ProposeOpen(OpenParams{Asset: invalidCredit, AssetLimit: "100"})
+	require.EqualError(t, err, `validation failed for *txnbuild.ChangeTrust operation: Field: Line, Error: asset code length must be between 1 and 12 characters`)
 
 	validCredit := CreditAsset{Code: "ABCD", Issuer: "GCSZIQEYTDI427C2XCCIWAGVHOIZVV2XKMRELUTUVKOODNZWSR2OLF6P"}
 	_, err = sendingChannel.ProposeOpen(OpenParams{Asset: validCredit, AssetLimit: ""})
-	require.Error(t, err)
-	require.Equal(t, "parsing asset limit: strconv.Atoi: parsing \"\": invalid syntax", err.Error())
+	require.EqualError(t, err, `parsing asset limit: strconv.Atoi: parsing "": invalid syntax`)
 
 	validCredit = CreditAsset{Code: "ABCD", Issuer: "GCSZIQEYTDI427C2XCCIWAGVHOIZVV2XKMRELUTUVKOODNZWSR2OLF6P"}
 	_, err = sendingChannel.ProposeOpen(OpenParams{Asset: validCredit, AssetLimit: "100"})
