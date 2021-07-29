af0394d3f5a7d5209f468b6b0fcecbe69163df73
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-07-29 05:18:21 +0000

    Remove deadcode

--- examples/console/util.go
+++ examples/console/util.go
@@ -8,18 +8,6 @@ import (
 	"github.com/stellar/go/txnbuild"
 )
 
-func getSeqNum(client horizonclient.ClientInterface, accountID string) (int64, error) {
-	account, err := client.AccountDetail(horizonclient.AccountRequest{AccountID: accountID})
-	if err != nil {
-		return 0, fmt.Errorf("getting account %s: %w", accountID, err)
-	}
-	seqNum, err := account.GetSequenceNumber()
-	if err != nil {
-		return 0, fmt.Errorf("getting sequence number of account %s: %w", accountID, err)
-	}
-	return seqNum, nil
-}
-
 func createAccountWithRoot(client horizonclient.ClientInterface, networkPassphrase string, accountKey *keypair.FromAddress) error {
 	rootKey := keypair.Root(networkPassphrase)
 	root, err := client.AccountDetail(horizonclient.AccountRequest{AccountID: rootKey.Address()})
