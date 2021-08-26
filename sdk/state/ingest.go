e9c1131d30c76ff40ca68393e3f6307a5782f8ac
Alec Charbonneau <aleccharb21@gmail.com>
2021-08-26 08:34:43 -0700

    Update sdk/state/ingest.go
    Co-authored-by: Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>

--- sdk/state/ingest.go
+++ sdk/state/ingest.go
@@ -178,7 +178,7 @@ func (c *Channel) ingestTxMetaToUpdateBalances(resultMetaXDR string) error {
 // transaction was submitted are in this channel's expected states to mark the
 // channel as open.
 func (c *Channel) ingestFormationTx(formationTx *txnbuild.Transaction, resultXDR string, resultMetaXDR string) (err error) {
-	// If the transaction is not the formation transaction, return.
+	// If the transaction is not the formation transaction, ignore.
 	myFormationTx, err := c.OpenTx()
 	if err != nil {
 		return fmt.Errorf("creating formation tx: %w", err)
