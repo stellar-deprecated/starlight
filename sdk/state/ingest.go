ccd86f5bff5c42b72f6a603c55ca253f3300108c
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-09-01 21:47:47 +0000

    remove fmt

--- sdk/state/ingest.go
+++ sdk/state/ingest.go
@@ -273,8 +273,6 @@ func (c *Channel) ingestFormationTx(tx *txnbuild.Transaction, resultXDR string, 
 	}
 
 	// Validate the initiator escrow account sequence number is correct.
-	fmt.Println("meta", initiatorEscrowAccountEntry.SeqNum)
-	fmt.Println("formation", formationTx.SequenceNumber())
 	if int64(initiatorEscrowAccountEntry.SeqNum) != formationTx.SequenceNumber() {
 		c.openExecutedWithError = fmt.Errorf("incorrect initiator escrow account sequence number found, found: %d want: %d",
 			int64(initiatorEscrowAccountEntry.SeqNum), formationTx.SequenceNumber())
