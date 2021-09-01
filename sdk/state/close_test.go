b141c698eaab6da358e207466415614fd49b9f44
Alec Charbonneau <aleccharb21@gmail.com>
2021-09-01 14:00:51 -0700

    gofumt

--- sdk/state/close_test.go
+++ sdk/state/close_test.go
@@ -229,5 +229,4 @@ func TestChannel_ProposeAndConfirmCoordinatedClose_rejectIfChannelNotOpen(t *tes
 
 	_, err = senderChannel.ConfirmClose(CloseAgreement{})
 	require.EqualError(t, err, "validating close agreement: cannot confirm a coordinated close before channel is opened")
-
 }
