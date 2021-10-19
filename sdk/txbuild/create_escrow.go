f4d197e4f16d1fbb18bdf3bcd12f332141d0ba1d
Alec Charbonneau <aleccharb21@gmail.com>
2021-10-19 15:39:57 -0700

    update go fork pointing to

--- sdk/txbuild/create_escrow.go
+++ sdk/txbuild/create_escrow.go
@@ -1,6 +1,7 @@
 package txbuild
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/stellar/go/amount"
@@ -12,7 +13,7 @@ type CreateEscrowParams struct {
 	Creator        *keypair.FromAddress
 	Escrow         *keypair.FromAddress
 	SequenceNumber int64
-	Asset          txnbuild.Asset
+	Asset          txnbuild.BasicAsset
 }
 
 func CreateEscrow(p CreateEscrowParams) (*txnbuild.Transaction, error) {
@@ -35,8 +36,13 @@ func CreateEscrow(p CreateEscrowParams) (*txnbuild.Transaction, error) {
 		},
 	}
 	if !p.Asset.IsNative() {
+		// Alec TODO - diff between changetrust asset and trustline asset?
+		asset, err := p.Asset.ToChangeTrustAsset()
+		if err != nil {
+			return nil, fmt.Errorf("getting change trust asset from nonnative asset: %w", err)
+		}
 		ops = append(ops, &txnbuild.ChangeTrust{
-			Line:          p.Asset,
+			Line:          asset,
 			Limit:         amount.StringFromInt64(math.MaxInt64),
 			SourceAccount: p.Escrow.Address(),
 		})
