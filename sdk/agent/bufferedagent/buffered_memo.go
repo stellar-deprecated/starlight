73d2cb157f029bff925f24de30f12f75e1fc85a5
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-02 12:53:23 -0700

    Disable gzip

--- sdk/agent/bufferedagent/buffered_memo.go
+++ sdk/agent/bufferedagent/buffered_memo.go
@@ -1,10 +1,11 @@
 package bufferedagent
 
 import (
-	"compress/gzip"
 	"encoding/gob"
 	"fmt"
 	"strings"
+
+	// "github.com/klauspost/compress/gzip"
 )
 
 type bufferedPaymentsMemo struct {
@@ -14,28 +15,30 @@ type bufferedPaymentsMemo struct {
 
 func (m bufferedPaymentsMemo) String() string {
 	sb := strings.Builder{}
-	z, err := gzip.NewWriterLevel(&sb, gzip.BestSpeed)
-	if err != nil {
-		panic(fmt.Errorf("creating gzip writer: %w", err))
-	}
+	z := &sb
+	// z, err := gzip.NewWriterLevel(&sb, gzip.BestSpeed)
+	// if err != nil {
+	// 	panic(fmt.Errorf("creating gzip writer: %w", err))
+	// }
 	enc := gob.NewEncoder(z)
-	err = enc.Encode(m)
+	err := enc.Encode(m)
 	if err != nil {
 		panic(fmt.Errorf("encoding buffered payments memo as json: %w", err))
 	}
-	z.Close()
+	// z.Close()
 	return sb.String()
 }
 
 func parseBufferedPaymentMemo(memo string) (bufferedPaymentsMemo, error) {
 	r := strings.NewReader(memo)
-	z, err := gzip.NewReader(r)
-	if err != nil {
-		return bufferedPaymentsMemo{}, fmt.Errorf("creating gzip reader: %w", err)
-	}
+	z := r
+	// z, err := gzip.NewReader(r)
+	// if err != nil {
+	// 	return bufferedPaymentsMemo{}, fmt.Errorf("creating gzip reader: %w", err)
+	// }
 	dec := gob.NewDecoder(z)
 	m := bufferedPaymentsMemo{}
-	err = dec.Decode(&m)
+	err := dec.Decode(&m)
 	if err != nil {
 		return bufferedPaymentsMemo{}, fmt.Errorf("decoding buffered payments memo from json: %w", err)
 	}
