626ec5061a4f678958f392ccd14b8ca7be5a78ed
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-02 15:25:54 -0700

    disable gzip

--- sdk/agent/bufferedagent/buffered_memo.go
+++ sdk/agent/bufferedagent/buffered_memo.go
@@ -4,8 +4,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
-
-	"github.com/klauspost/compress/gzip"
+	// "github.com/klauspost/compress/gzip"
 )
 
 type bufferedPaymentsMemo struct {
@@ -15,29 +14,29 @@ type bufferedPaymentsMemo struct {
 
 func (m *bufferedPaymentsMemo) MarshalBinary() ([]byte, error) {
 	b := bytes.Buffer{}
-	z, err := gzip.NewWriterLevel(&b, gzip.BestSpeed)
-	if err != nil {
-		panic(fmt.Errorf("creating gzip writer: %w", err))
-	}
-	enc := gob.NewEncoder(z)
+	// z, err := gzip.NewWriterLevel(&b, gzip.BestSpeed)
+	// if err != nil {
+	// 	panic(fmt.Errorf("creating gzip writer: %w", err))
+	// }
+	enc := gob.NewEncoder(&b)
 	type bpm bufferedPaymentsMemo
-	err = enc.Encode((*bpm)(m))
+	err := enc.Encode((*bpm)(m))
 	if err != nil {
 		return nil, fmt.Errorf("encoding buffered payments memo: %w", err)
 	}
-	z.Close()
+	// z.Close()
 	return b.Bytes(), nil
 }
 
 func (m *bufferedPaymentsMemo) UnmarshalBinary(b []byte) error {
 	r := bytes.NewReader(b)
-	z, err := gzip.NewReader(r)
-	if err != nil {
-		return fmt.Errorf("creating gzip reader: %w", err)
-	}
-	dec := gob.NewDecoder(z)
+	// z, err := gzip.NewReader(r)
+	// if err != nil {
+	// 	return fmt.Errorf("creating gzip reader: %w", err)
+	// }
+	dec := gob.NewDecoder(r)
 	type bpm bufferedPaymentsMemo
-	err = dec.Decode((*bpm)(m))
+	err := dec.Decode((*bpm)(m))
 	if err != nil {
 		return fmt.Errorf("decoding buffered payments memo: %w", err)
 	}
