9d170b7a354c09e74a27592da62d99eccd1e3171
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-03 17:31:25 -0700

    diag

--- sdk/state/doc.go
+++ sdk/state/doc.go
@@ -24,7 +24,6 @@ signatures.
         |                  |
      Propose               |
         +----------------->+
-        |                  |
         |               Confirm
         +<-----------------+
     Finalize               |
