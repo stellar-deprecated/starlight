b3938cdee1cedc677bec939fad8a2ce61f4f2f6e
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-08 22:17:38 -0800

    sdk/agent/horizon: add docs

--- sdk/agent/horizon/sequence_number_collector.go
+++ sdk/agent/horizon/sequence_number_collector.go
@@ -5,12 +5,18 @@ import (
 
 	"github.com/stellar/go/clients/horizonclient"
 	"github.com/stellar/go/keypair"
+	"github.com/stellar/starlight/sdk/agent"
 )
 
+var _ agent.SequenceNumberCollector = &SequenceNumberCollector{}
+
+// SequenceNumberCollector implements an agent's interface for collecting the
+// current sequence number by querying Horizon's accounts endpoint.
 type SequenceNumberCollector struct {
 	HorizonClient horizonclient.ClientInterface
 }
 
+// GetSequenceNumber queries Horizon for the balance of the given account.
 func (h *SequenceNumberCollector) GetSequenceNumber(accountID *keypair.FromAddress) (int64, error) {
 	account, err := h.HorizonClient.AccountDetail(horizonclient.AccountRequest{AccountID: accountID.Address()})
 	if err != nil {
