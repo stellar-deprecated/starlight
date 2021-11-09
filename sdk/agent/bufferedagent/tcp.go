95e7cefd0c3322ed0cee1acd033f350c7d6d178b
Leigh McCulloch <351529+leighmcculloch@users.noreply.github.com>
2021-11-08 22:34:29 -0800

    sdk/agent/bufferedagent: add docs

--- sdk/agent/bufferedagent/tcp.go
+++ sdk/agent/bufferedagent/tcp.go
@@ -1,10 +1,14 @@
 package bufferedagent
 
+// ServeTCP listens on the given address for a single incoming connection to
+// start a payment channel.
 func (a *Agent) ServeTCP(addr string) error {
 	go a.eventLoop()
 	return a.agent.ServeTCP(addr)
 }
 
+// ConnectTCP connects to the given address for establishing a single payment
+// channel.
 func (a *Agent) ConnectTCP(addr string) error {
 	go a.eventLoop()
 	return a.agent.ConnectTCP(addr)
